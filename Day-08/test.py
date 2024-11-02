67aa3bb878b90919f016fc0e2974ac5b8fc75f5d
sairam kalla <sairamkalla26@gmail.com>
2024-11-02 13:46:15 +0000

    mode

---
+++ Day-08/test.py
@@ -0,0 +1,15 @@
+#Lists
+
+students_names = ["sai", "ram", "jai", "kalla"]
+students_names.append("veda")
+students_names.remove("jai")
+print(students_names)
+
+print(students_names[1])
+print(len(students_names))
+
+print(students_names[0:1])
+print(students_names[0:2])
+
+new_list = students_names[0:2]
+print(students_names[0:1])
---
+++ Day-08/test_sort.py
@@ -0,0 +1,5 @@
+#sorting
+
+numbers = [4, 1, 3, 2, 5]
+numbers.sort()
+print(numbers)
