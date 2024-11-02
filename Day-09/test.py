f5c3d06ee9de4b45f59b93566b0232ec45d991aa
sairam kalla <sairamkalla26@gmail.com>
2024-11-02 16:13:27 +0000

    mode

---
+++ Day-09/test.py
@@ -0,0 +1,13 @@
+print("abhishek") #copying and printing the same is complicated
+print("abhishek")
+print("abhishek")
+print("abhishek")
+
+for i in range(10):   #used loop here avoiding copy
+    print("abhishek")
+    print(i)    #sequence indexing
+
+colors = ["yellow", "green", "blue"] #lists
+
+for x in colors:
+    print(x)
---
+++ Day-09/test_loop_manipulation.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    if number == 3:
+        break           #will break the execution
+    print(number)
+
+digits = [6, 7, 8, 9]
+for digit in digits:
+    if digit == 7:
+        continue        #will continue with the execution after stopping at 3
+    print(digit)
